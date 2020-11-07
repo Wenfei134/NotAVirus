c779904381f9c9c1f0dba5e08b6db5cda77a2181
harris222 <harris222@users.noreply.github.com>
2020-11-07 13:46:11 -0800

    Model?ready?

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
+import tensorflow.keras.layers as layers
+import tensorflow.keras.models as models
 import numpy as np
 import cv2
 import os
 import sys
+from tqdm import tqdm
+from matplotlib.pyplot import plt
 from sklearn.model_selection import train_test_split
 # import matplotlib.pyplot as plt
 
@@ -22,18 +26,21 @@ def main():
     labels = tf.keras.utils.to_categorical(labels) 
     images = np.array(images)
     labels = np.array(labels)
+
+    images = images.reshape(images.shape[0], images.shape[1], images.shape[2], 1)
     X_train, X_test, Y_train, Y_test = train_test_split(images, labels, test_size=0.2)
-
-    # print(X_train)
-
+    # print(X_train.shape)
+    # print(Y_train.shape)
+    # sys.exit(0)
     # Build the CNN
-    # model = build_model()
+    n_labels = labels.shape[1]
+    model = build_model(n_labels)
+    train_model(model, X_test, Y_test, X_train, Y_train)
 
     # # Fit
     # model.fit(X_train, Y_train, epochs=EPOCHS)
 
     # Evaluate
-    return
 
     
 def load_images(pos_dir, neg_dir):
@@ -43,9 +50,8 @@ def load_images(pos_dir, neg_dir):
     '''
     images = []
     labels = []
-
     # Load postive images with opencv, and label as 1
-    for image in os.listdir(pos_dir):
+    for image in tqdm(os.listdir(pos_dir)):
         path = os.path.join(pos_dir, image)
         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) #def: read in 
         try:
@@ -56,7 +62,7 @@ def load_images(pos_dir, neg_dir):
             print(str(e))
     
     # Load negative images, label = 0
-    for image in os.listdir(neg_dir):
+    for image in tqdm(os.listdir(neg_dir)):
         path = os.path.join(neg_dir, image)
         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) #def: read in
         try:
@@ -70,19 +76,39 @@ def load_images(pos_dir, neg_dir):
 
 
 
-def build_model():
+def build_model(n_labels):
     """
     Create CNN
     """
-    INPUTSHAPE = (HEIGHT, WIDTH)
-    # model = models.Seq
-    
+    INPUTSHAPE = (HEIGHT, WIDTH, 1)
+    model = models.Sequential([
+        layers.Conv2D(16, (5,5), padding='same', activation='relu', input_shape=INPUTSHAPE),
+        layers.Conv2D(16, (5,5), padding='same', activation='relu'),
+        layers.MaxPool2D((2,2)),
+        layers.Dropout(0.15),
+        layers.Conv2D(32, (5,5), padding='same', activation='relu'),
+        layers.Conv2D(32, (5,5), padding='same', activation='relu'),
+        layers.MaxPool2D((2,2)),
+        layers.Dropout(0.15),
+        layers.Flatten(),
+        layers.Dense(256, activation='relu'),
+        layers.Dense(n_labels, activation = 'softmax')
+    ])
 
-
-
-
-
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(model.summary())
     return model
+
+def train_model(model, x_test, y_test, x_train, y_train):
+    history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
 
 if __name__ == "__main__":
     main()
