06852f7344cbdcfc742bb7c9de82c88036894cee
Ian <IanWebster2828@gmail.com>
2020-11-08 07:16:33 -0500

    Classify image returns confidence and result of covid cough sample

--- classify_image.py
+++ classify_image.py
@@ -17,6 +17,7 @@ NEG_DIR = "./proc_negative_Covid-19"
 # Uses the serialized model generated from main.py to classify mel spectrograms.
 # Will return a label representing covid/not covid
 def classify_image(filepath):
+    final_pred = {}
     # Load in the model with a test sample
     reconstructed_model = tf.keras.models.load_model("finalized_model_c") #finalized_model_c is the best so far
     img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
@@ -24,21 +25,24 @@ def classify_image(filepath):
     test_input = np.array(test_input)
 
     test_input = test_input.reshape(1, test_input.shape[0], test_input.shape[1], 1)
-    print(test_input.shape)
-    print(reconstructed_model.predict(test_input))
+    # print(test_input.shape)
+    prediction = reconstructed_model.predict(test_input)
+    # print(prediction)
+
+    if prediction[0, 0] > prediction[0, 1]:
+        final_pred["result"] = 'negative'
+        final_pred["confidence"] = prediction[0, 0] * 100
+    else:
+        final_pred["result"] = 'positive'
+        final_pred["confidence"] = prediction[0, 1] * 100
+    
+    # for field in final_pred:
+    #     print(final_pred[field])
+    return final_pred
 
 
-# classify_image(os.path.join(POS_DIR, "positive_0_3.jpg")) # test
-# classify_image(os.path.join(POS_DIR, "positive_0_23.jpg")) # test
-# classify_image(os.path.join(POS_DIR, "positive_0_156.jpg")) # test
-# classify_image(os.path.join(NEG_DIR, "negative_0_710.jpg")) # test
-# classify_image(os.path.join(NEG_DIR, "negative_0_466.jpg")) # test
-# classify_image(os.path.join(NEG_DIR, "negative_0_508.jpg")) # test
-# classify_image(os.path.join(NEG_DIR, "negative_0_771.jpg")) # test
-
-
-# Bulk test
-# dirs = [ NEG_DIR] #POS_DIR,
+# # Bulk test
+# dirs = [NEG_DIR] #POS_DIR,
 
 # for dir in dirs:
 #     # Rotate all images
