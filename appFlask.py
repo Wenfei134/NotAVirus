fbcd7aac8515e3c7a72f9d0d8117e3e5753fa98a
Wenfei134 <60166421+Wenfei134@users.noreply.github.com>
2020-11-08 03:38:11 -0800

    added 16 mb limit to file inputs

--- appFlask.py
+++ appFlask.py
@@ -3,7 +3,8 @@ from getSpectrogram import submitAudio
 import os
 
 app = Flask(__name__, static_folder='./frontend/build/static/', template_folder='./frontend/build/')
-UPLOAD_FOLDER = './'
+app.config['UPLOAD_FOLDER'] = './'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/')
 def home():
