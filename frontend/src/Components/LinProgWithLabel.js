c9e1075cdbe49ffea21c3e2b070eadbbaa75c641
harris222 <harris222@users.noreply.github.com>
2020-11-07 21:16:56 -0800

    YEEET THIS IS THE FRONTEND

---
+++ frontend/src/Components/LinProgWithLabel.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function LinearProgressWithLabel(props) {
+    return (
+      <Box display="flex" alignItems="center" width="80%">
+        <Box width="100%" mr={1}>
+          <LinearProgress variant="determinate" {...props} />
+        </Box>
+        <Box minWidth={35}>
+          <Typography variant="body2" color="textSecondary">{`${Math.round(
+            props.value,
+          )}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
+  
+  LinearProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate and buffer variants.
+     * Value between 0 and 100.
+     */
+    value: PropTypes.number.isRequired,
+  };
+
+  export default LinearProgressWithLabel;
