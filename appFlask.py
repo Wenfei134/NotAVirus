9fc199eea6906899cdd6a52504efab6d78a53303
Ian <IanWebster2828@gmail.com>
2020-11-08 06:52:41 -0500

    Found best model so far. finalized_model_c. 82% roughly

--- appFlask.py
+++ appFlask.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from getSpectrogram import submitAudio
+from classify_image import classify_image
 
 app = Flask(__name__, static_folder='./frontend/build/static/', template_folder='./frontend/build/')
 
@@ -12,7 +13,9 @@ def prediction():
     if request.method == 'POST': 
         audio = request.files['audiofile']
         success = submitAudio( audio )
-        return render_template('prediction.html', output = success )
+        if success:
+            isCovid = classify_image( "./audio.jpg")
+        return render_template('prediction.html', isCovid=isCovid )
 
 
 if __name__=="__main__":
--- classify_image.py
+++ classify_image.py
@@ -11,20 +11,38 @@ HEIGHT_IN = 240 #288
 WIDTH = int(WIDTH_IN/2)
 HEIGHT = int(HEIGHT_IN/2)
 
-POS_DIR = "./proc_positive_Covid-19"
+POS_DIR = "./proc_positive_Covid-19" #NOTE: this dataset has been reduced to end at positive_0_2094.jpg (336 of the original positives - to match the number of negatives)
+NEG_DIR = "./proc_negative_Covid-19"
 
 # Uses the serialized model generated from main.py to classify mel spectrograms.
 # Will return a label representing covid/not covid
 def classify_image(filepath):
     # Load in the model with a test sample
-    reconstructed_model = tf.keras.models.load_model("finalized_model")
+    reconstructed_model = tf.keras.models.load_model("finalized_model_c") #finalized_model_c is the best so far
     img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     test_input = cv2.resize(img, (WIDTH, HEIGHT), interpolation=cv2.INTER_LINEAR)
     test_input = np.array(test_input)
 
-    test_input = test_input.reshape( 1, test_input.shape[0], test_input.shape[1], 1)
+    test_input = test_input.reshape(1, test_input.shape[0], test_input.shape[1], 1)
     print(test_input.shape)
     print(reconstructed_model.predict(test_input))
 
 
-classify_image(os.path.join(POS_DIR, "positive_0_3.jpg")) # test
+# classify_image(os.path.join(POS_DIR, "positive_0_3.jpg")) # test
+# classify_image(os.path.join(POS_DIR, "positive_0_23.jpg")) # test
+# classify_image(os.path.join(POS_DIR, "positive_0_156.jpg")) # test
+# classify_image(os.path.join(NEG_DIR, "negative_0_710.jpg")) # test
+# classify_image(os.path.join(NEG_DIR, "negative_0_466.jpg")) # test
+# classify_image(os.path.join(NEG_DIR, "negative_0_508.jpg")) # test
+# classify_image(os.path.join(NEG_DIR, "negative_0_771.jpg")) # test
+
+
+# Bulk test
+# dirs = [ NEG_DIR] #POS_DIR,
+
+# for dir in dirs:
+#     # Rotate all images
+#     for img_name in os.listdir(dir):
+#         path = os.path.join(dir, img_name)
+#         classify_image(path)
+        
